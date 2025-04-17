9afed41c0eb1e0a9153b06589ad0059c6395f9ef
Nigorigo-TC <nigorigo.training@gmail.com>
2025-04-17 10:22:13 +0900

    Update survey6.py

--- survey6.py
+++ survey6.py
@@ -201,8 +201,7 @@ if not is_admin:
        options = [None] + list(range(0, 301))
        exercise_time = st.select_slider("**22. トレーニング時間（分）**",options=options,value=None,format_func=lambda x: "選択してください" if x is None else f"{x} 分",key="exercise_time")
 
-        
-　　　　st.image("rpe_chart.png", caption="運動のきつさ（0～10）", use_container_width=True)
+        st.image("rpe_chart.png", caption="運動のきつさ（0～10）", use_container_width=True)
         exercise_rpe = st.selectbox("**23. 運動のきつさ（RPE）**", list(range(0, 11)), key="exercise_rpe")
         st.caption("※画像を参考に運動のきつさ（RPE）を選択してください")
 
